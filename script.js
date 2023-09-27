8e54f8df8e13ca09997322f487dcc4df144b6d41
jonckheerethijs <jonckheerethijs@gmail.com>
2023-09-27 16:31:11 +0200

    completed functionality for button
    button now gives alerts and new prompts when unsupported values are
    filled in

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createColumn(col) {
     const newCol = document.createElement("div")
     newCol.classList.add('grid', 'column')
     newCol.setAttribute('id', `column-${col}`)
-    container.appendChild(newCol)
+    gridContainer.appendChild(newCol)
 }
 
 function createCell(col, row) {
@@ -25,22 +25,52 @@ function createGrid(col, row) {
     }
 }
 
-const container = document.querySelector('.grid-container')
-
-function buttonPress(){
-    const x = prompt("How many columns do you want?",10)
-    const y = prompt("How many rows do you want?",10)
-    createGrid(25,25)
-}
-
-function changeBackground(e){
-    e.target.classList.toggle('background')
-}
+const gridContainer = document.querySelector('.grid-container')
+createGrid(16, 16)
 
 // UI
 
-const cells = Array.from(document.querySelectorAll('.cell'))
-cells.forEach(key => key.addEventListener("mouseover",changeBackground))
+function changeBackground(e) {
+    e.target.classList.toggle('background')
+}
 
-const resetButton = document.getElementById('reset-button')
-resetButton.addEventListener("click", buttonPress)
+function removeGrid() {
+    const toRemove = Array.from(gridContainer.getElementsByClassName("column"))
+    toRemove.forEach(key => key.remove())
+}
+
+function checkInput(input){
+    if (input>100){
+        alert (`${input} is larger than 100`)
+        return true
+    } else if (isNaN(input)){
+        console.log(isNaN(input))
+        alert(`${input} is not a number`)
+        return true
+    } else {
+        return false
+    }
+}
+
+function buttonPress() {
+    removeGrid()
+    let x = prompt("How many columns do you want? (Max 100)", 10)
+    while (checkInput(x)){
+        x = prompt("How many columns do you want? (Max 100)", 10)
+        checkInput(x)
+    }
+    const y = prompt("How many rows do you want? (Max 100)", 10)
+    while (checkInput(y)){
+        y = prompt("How many columns do you want? (Max 100)", 10)
+        checkInput(y))
+    }
+    createGrid(x, y)
+    const cells = Array.from(document.querySelectorAll('.cell'))
+    cells.forEach(key => key.addEventListener("mouseover", changeBackground))
+}
+
+const cells = Array.from(document.querySelectorAll('.cell'))
+cells.forEach(key => key.addEventListener("mouseover", changeBackground))
+
+//const resetButton = document.getElementById('reset-button')
+//resetButton.addEventListener("click", buttonPress)
